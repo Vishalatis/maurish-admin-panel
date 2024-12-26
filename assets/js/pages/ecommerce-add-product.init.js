af0ac99db7900c8ae0c3277709938afa42db2674
niraj narale <vishal.atpuae@gmail.com>
2024-12-26 17:09:39 +0530

    first

--- assets/js/pages/ecommerce-add-product.init.js
+++ assets/js/pages/ecommerce-add-product.init.js
@@ -1,12 +1,60 @@
-$(document).ready(function() {
-    $("#addproduct-nav-pills-wizard").bootstrapWizard({
-        tabClass: "nav nav-pills nav-justified"
-    })
-}), $(".select2").select2();
-var triggerTabList = [].slice.call(document.querySelectorAll(".twitter-bs-wizard-nav .nav-link"));
-triggerTabList.forEach(function(t) {
-    var a = new bootstrap.Tab(t);
-    t.addEventListener("click", function(t) {
-        t.preventDefault(), a.show()
-    })
+// Initialize select2 elements
+$(".select2").select2();
+
+// Initialize Bootstrap tabs
+document
+  .querySelectorAll(".twitter-bs-wizard-nav .nav-link")
+  .forEach(function (tab) {
+    var bootstrapTab = new bootstrap.Tab(tab);
+    tab.addEventListener("click", function (event) {
+      event.preventDefault();
+      bootstrapTab.show();
+    });
+  });
+
+// Document ready function
+$(document).ready(function () {
+  const tabs = $(".twitter-bs-wizard-tab-content .tab-pane");
+  let currentIndex = 0;
+
+  // Function to update the active tab
+  function updateTab(index) {
+    tabs.removeClass("active show");
+    $(tabs[index]).addClass("active show");
+    updateButtonState(index);
+  }
+
+  // Function to enable/disable Previous/Next buttons
+  function updateButtonState(index) {
+    const totalTabs = tabs.length;
+    $(".twitter-bs-wizard-pager-link .previous a").toggleClass(
+      "disabled",
+      index === 0
+    );
+    $(".twitter-bs-wizard-pager-link .next a").toggleClass(
+      "disabled",
+      index === totalTabs - 1
+    );
+  }
+
+  // Handle Next button click
+  $(".twitter-bs-wizard-pager-link .next a").on("click", function (e) {
+    e.preventDefault();
+    if (currentIndex < tabs.length - 1) {
+      currentIndex++;
+      updateTab(currentIndex);
+    }
+  });
+
+  // Handle Previous button click
+  $(".twitter-bs-wizard-pager-link .previous a").on("click", function (e) {
+    e.preventDefault();
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateTab(currentIndex);
+    }
+  });
+
+  // Initialize button states
+  updateButtonState(currentIndex);
 });
